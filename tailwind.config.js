bdd2c733e7b4dc7d8a438aa7ceccc4eae2231776
friduby <friduby@gmail.com>
2022-06-04 02:50:47 +0430

    project seems finish

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#141519",
+        secondary: "#222933",
+        tertiary: "#2e3742",
+        hoverButtons: "#F9740E"
+      }
+    },
+  },
+  plugins: [],
+}
